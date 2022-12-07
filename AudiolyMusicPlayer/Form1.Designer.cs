4fc250a5b04180a8377eae03c30c150691f0de92
Nick Hill <nicholaspatrickhill@gmail.com>
2022-12-06 21:47:45 -0500

    added buttons

--- AudiolyMusicPlayer/Form1.Designer.cs
+++ AudiolyMusicPlayer/Form1.Designer.cs
@@ -30,14 +30,22 @@
         {
             this.btn_preview = new System.Windows.Forms.Button();
             this.btn_next = new System.Windows.Forms.Button();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.p_bar = new System.Windows.Forms.ProgressBar();
+            this.track_list = new System.Windows.Forms.ListBox();
+            this.pic_art = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_art)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_preview
             // 
             this.btn_preview.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_preview.Location = new System.Drawing.Point(12, 393);
+            this.btn_preview.Location = new System.Drawing.Point(39, 388);
             this.btn_preview.Name = "btn_preview";
-            this.btn_preview.Size = new System.Drawing.Size(75, 23);
+            this.btn_preview.Size = new System.Drawing.Size(75, 29);
             this.btn_preview.TabIndex = 0;
             this.btn_preview.Text = "Preview";
             this.btn_preview.UseVisualStyleBackColor = true;
@@ -45,31 +53,112 @@
             // btn_next
             // 
             this.btn_next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_next.Location = new System.Drawing.Point(93, 393);
+            this.btn_next.Location = new System.Drawing.Point(120, 388);
             this.btn_next.Name = "btn_next";
-            this.btn_next.Size = new System.Drawing.Size(75, 23);
+            this.btn_next.Size = new System.Drawing.Size(75, 29);
             this.btn_next.TabIndex = 1;
             this.btn_next.Text = "Next";
             this.btn_next.UseVisualStyleBackColor = true;
             // 
+            // btn_play
+            // 
+            this.btn_play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_play.Location = new System.Drawing.Point(201, 388);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(75, 29);
+            this.btn_play.TabIndex = 2;
+            this.btn_play.Text = "Play";
+            this.btn_play.UseVisualStyleBackColor = true;
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_pause.Location = new System.Drawing.Point(282, 388);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.Size = new System.Drawing.Size(75, 29);
+            this.btn_pause.TabIndex = 3;
+            this.btn_pause.Text = "Pause";
+            this.btn_pause.UseVisualStyleBackColor = true;
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_stop.Location = new System.Drawing.Point(363, 388);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 29);
+            this.btn_stop.TabIndex = 4;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            // 
+            // btn_open
+            // 
+            this.btn_open.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_open.Location = new System.Drawing.Point(444, 388);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(154, 29);
+            this.btn_open.TabIndex = 5;
+            this.btn_open.Text = "Open";
+            this.btn_open.UseVisualStyleBackColor = true;
+            // 
+            // p_bar
+            // 
+            this.p_bar.Location = new System.Drawing.Point(39, 348);
+            this.p_bar.Name = "p_bar";
+            this.p_bar.Size = new System.Drawing.Size(559, 23);
+            this.p_bar.TabIndex = 6;
+            // 
+            // track_list
+            // 
+            this.track_list.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.track_list.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.track_list.FormattingEnabled = true;
+            this.track_list.ItemHeight = 16;
+            this.track_list.Location = new System.Drawing.Point(201, 173);
+            this.track_list.Name = "track_list";
+            this.track_list.Size = new System.Drawing.Size(397, 148);
+            this.track_list.TabIndex = 7;
+            // 
+            // pic_art
+            // 
+            this.pic_art.Image = global::AudiolyMusicPlayer.Properties.Resources._45rpm;
+            this.pic_art.Location = new System.Drawing.Point(39, 173);
+            this.pic_art.Name = "pic_art";
+            this.pic_art.Size = new System.Drawing.Size(148, 148);
+            this.pic_art.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_art.TabIndex = 8;
+            this.pic_art.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(845, 428);
+            this.ClientSize = new System.Drawing.Size(644, 437);
+            this.Controls.Add(this.pic_art);
+            this.Controls.Add(this.track_list);
+            this.Controls.Add(this.p_bar);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_pause);
+            this.Controls.Add(this.btn_play);
             this.Controls.Add(this.btn_next);
             this.Controls.Add(this.btn_preview);
             this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Audioly Music Player";
+            ((System.ComponentModel.ISupportInitialize)(this.pic_art)).EndInit();
             this.ResumeLayout(false);
 
         }
+
 
         #endregion
 
         private System.Windows.Forms.Button btn_preview;
         private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.Button btn_pause;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.ProgressBar p_bar;
+        private System.Windows.Forms.ListBox track_list;
+        private System.Windows.Forms.PictureBox pic_art;
     }
 }
 
