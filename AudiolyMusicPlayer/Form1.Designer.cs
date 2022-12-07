209400577e9684d7fe84518fc7e3ed0150a91823
Nick Hill <nicholaspatrickhill@gmail.com>
2022-12-06 21:22:38 -0500

    initial commit

---
+++ AudiolyMusicPlayer/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace AudiolyMusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_preview = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_preview
+            // 
+            this.btn_preview.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_preview.Location = new System.Drawing.Point(12, 393);
+            this.btn_preview.Name = "btn_preview";
+            this.btn_preview.Size = new System.Drawing.Size(75, 23);
+            this.btn_preview.TabIndex = 0;
+            this.btn_preview.Text = "Preview";
+            this.btn_preview.UseVisualStyleBackColor = true;
+            // 
+            // btn_next
+            // 
+            this.btn_next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_next.Location = new System.Drawing.Point(93, 393);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(75, 23);
+            this.btn_next.TabIndex = 1;
+            this.btn_next.Text = "Next";
+            this.btn_next.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 428);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_preview);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Audioly Music Player";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_preview;
+        private System.Windows.Forms.Button btn_next;
+    }
+}
+
